d9bc60ae02a158fceed3277486e74f3b559c58a8
matt.clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2019-04-12 12:40:22 +0100

    more stuff to save

--- routes/issue.route.js
+++ routes/issue.route.js
@@ -4,7 +4,6 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const issue_controller = require('../controllers/issue.controller');
 
+router.post('/create', issue_controller.issue_create);
 
-// a simple test url to check that all of our files are communicating correctly.
-router.get('/test', issue_controller.test);
 module.exports = router;
