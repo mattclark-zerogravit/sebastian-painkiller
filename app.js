5fb3e223fc210fa2b8d2e8bddb8410cdde5dd36f
matt.clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2019-04-12 12:30:35 +0100

    adding to share

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
-// initialize our express app
+const issue = require('./routes/issue.route'); // Imports routes for the products
 const app = express();
-
+app.use('/issue', issue);
 let port = 1234;
-
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
