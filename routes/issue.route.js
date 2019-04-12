08beeaf121bc0825c1fb7b112e76efd41d437736
matt.clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2019-04-12 13:21:57 +0100

    changed tact to meteor

--- routes/issue.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Require the controllers WHICH WE DID NOT CREATE YET!!
-const issue_controller = require('../controllers/issue.controller');
-
-router.post('/create', issue_controller.issue_create);
-
-module.exports = router;
